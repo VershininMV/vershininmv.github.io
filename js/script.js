62b6cd946e6bbd7189a03044686b04a914c8396b
VershininMV <vershininmv.1990@gmail.com>
2025-07-14 12:11:44 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-// Активация гамбургер-меню
 document.querySelector('.mobile-menu-toggle').addEventListener('click', function() {
   document.querySelector('.nav').classList.toggle('active');
 });
 
-// Закрытие меню при клике на ссылку
 document.querySelectorAll('.nav-link').forEach(link => {
   link.addEventListener('click', () => {
     document.querySelector('.nav').classList.remove('active');
